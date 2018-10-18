8bfe2abf7d13b257cb346a918f36cf45c5821ad6
Tom Perrine <tom.perrine@gmail.com>
2018-10-18 08:18:48 -0700

    take out anything not needed to make a regular Ubuntu instance

--- launch-multics.sh
+++ launch-multics.sh
@@ -2,14 +2,15 @@
 
 # given a GCP, etc account and the SDK on the install-from host, build and install a new server
 
-# Pre-rews:
-# Billing
+# we're going to use "oslogin" so set a username
+CLOUD_USERNAME=tom_perrine_gmail_com
 
-. ./set-private-data.sh
+# Set project information
+PROJ=new-cloud-server
+gcloud config set project ${PROJ}
 
-# If you don't use ssh-add to add your key to your active ssh-agent
+# If you didn't use ssh-add to add your key to your active ssh-agent
 # you're going to be typing your passphrase an awful lot
-
 
 # set specifics for this instance
 # this file sets all the non-sensitive customizations
@@ -18,24 +19,17 @@
 # pick a region
 export CLOUDSDK_COMPUTE_ZONE="us-central1-f"
 
+#
 # set information for the instance we will create
+#
 INSTANCENAME="my-multics-test"
 MACHINETYPE="f1-micro"
 IMAGEFAMILY="ubuntu-1804-lts"
 IMAGEPROJECT="ubuntu-os-cloud"
-# END OF CONFIG DATA
-
 #
 # network config data
 #
 # NOTE: this is not idempotent, can only be done once so check for existance first
-
-
-if [[ `gcloud compute firewall-rules list --filter="name=allow-6180" --format='table(name)' | tail -1 | awk '{print $1}'` != 'allow-6180'   ]]; then
-    echo "making firewall rule allow-6180"
-    gcloud compute firewall-rules create allow-6180 --allow=tcp:6180 --description="allow inbound 6180 for TELNET"  --source-ranges="0.0.0.0/0"
-fi
-
 #
 # create the instance
 #
@@ -48,13 +42,13 @@ gcloud compute instances add-metadata ${INSTANCENAME} --metadata enable-oslogin=
 #
 # it can take some time, and sometimes(?) the create returns much faster than expected, or the system
 # takes a long time to boot and get to the SSH server, so wait for it to be READY
-SSHRETURN="dummy"
-while [[ "RUNNING" != ${SSHRETURN} ]]; do
-    SSHRETURN=`gcloud compute instances describe ${INSTANCENAME} | grep status: | awk -F\  ' {print $2}' `
+#
+# first wait for the instance to be "running?
+STATERETURN="dummy"
+while [[ "RUNNING" != ${STATERETURN} ]]; do
+    STATERETURN=`gcloud compute instances describe ${INSTANCENAME} | grep status: | awk -F\  ' {print $2}' `
     sleep 5
-#    echo sshreturn ${SSHRETURN}
 done
-#echo ${SSHRETURN}
 
 #
 # now wait until the SSH server is running (we get a response without a timeout)
@@ -65,14 +59,13 @@ while [[ ${SSHRETURN} -ne 0 ]]; do
     sleep 3
 done
 
-#
-# here's how to get the internal and external IP addresses for all instances
-echo "internal address: " `gcloud --format="value(networkInterfaces[0].networkIP)"  compute instances list`
-echo "external IP address: " `gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)"  compute instances list`
+# if you don't do this then a lot of the application packages won't install properly
+# beside, its always a good idea to update+upgrade immediately after installation
+gcloud compute ssh ${CLOUD_USERNAME}@${INSTANCENAME} --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE} -- sudo apt-get --yes update
+gcloud compute ssh ${CLOUD_USERNAME}@${INSTANCENAME} --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE} -- sudo apt-get --yes upgrade
 
-# or for just the known instance
-echo just this instance
-gcloud --format="value(networkInterfaces[0].networkIP)"  compute instances describe ${INSTANCENAME} --zone ${CLOUDSDK_COMPUTE_ZONE}
+exit
+
 
 
 # put the main install script on the host and run it
