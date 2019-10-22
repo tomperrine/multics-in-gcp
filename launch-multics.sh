95f8d9b7bbd1c92f6fde7b756aee94693222bf8f
Tom Perrine <tom.perrine@gmail.com>
2019-10-21 21:43:48 -0700

    clean up installation script, fix some comments test for instance existance before trying to create it

--- launch-multics.sh
+++ launch-multics.sh
@@ -7,6 +7,14 @@
 
 . ./instance-name.sh
 
+# does the instance we're going to create already exist?
+# Note - the command doesn't return a usable exit code so we have to test separately
+tt=`gcloud compute instances list --filter="name=($INSTANCENAME)" | grep $INSTANCENAME | wc -l `
+if [ $tt != '0' ] ; then
+    echo "instance $INSTANCENAME already exists... exiting"
+    exit 1
+fi
+
 # launch the instance - assume it does NOT already exist
 # on a tiny instance, this will take up to 10 minutes to create the instance,
 # install Ubutnu and apply all patches
@@ -14,20 +22,23 @@
 echo "launching instance, patching (can take up to 10 minutes on a tiny)..."
 ../create-simple-google-instance/create-instance.sh
 
-# put the main install script on the host and run it
-# also put the customized Multics configure.ini there for use at Multics boot time
-# dont return here until the OS is running, all packages have been installed and
-# Multics has started (and halted)
+# put two files on the running instance:
 
-# this is the configuration script that is customized to include any
-# user accounts, projects, hostname, etc for your site
-# EDIT configure.ini BEFORE running this script
-gcloud compute scp configure.ini ${CLOUD_USERNAME}@${INSTANCENAME}: --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE}
-
+# the main install script - 5-minute-multics.sh
 # this script will update Ubuntu, install all the packages needed to build SIMH
 # then it will biuld SIMH, boot Multics and run the initial configuration script
 
-gcloud compute scp 5-minute-multics.sh ${CLOUD_USERNAME}@${INSTANCENAME}: --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE}
+# the initial Multics configuration file - configure.ini
+# this is the configuration script that is customized to include any
+# user accounts, projects, hostname, etc for your site
+# EDIT configure.ini BEFORE running this script
+
+# dont return here until the OS is running, all packages have been installed and
+# Multics has started (and halted)
+
+gcloud compute scp 5-minute-multics.sh configure.ini ${CLOUD_USERNAME}@${INSTANCENAME}: --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE}
+
+
 gcloud compute ssh ${CLOUD_USERNAME}@${INSTANCENAME} --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE} -- chmod +x 5-minute-multics.sh
 
 gcloud compute ssh ${CLOUD_USERNAME}@${INSTANCENAME} --project ${PROJ} --zone ${CLOUDSDK_COMPUTE_ZONE} -- './5-minute-multics.sh'
